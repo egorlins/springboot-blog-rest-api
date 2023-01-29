eb98dacf77087211eb1b385dece609b2959c80af
egorlins <45432230+egorlins@users.noreply.github.com>
2023-01-29 18:56:05 +0200

    Added get all comments and get comment by id

--- src/main/java/com/springboot/blog/service/CommentService.java
+++ src/main/java/com/springboot/blog/service/CommentService.java
@@ -1,7 +1,22 @@
 package com.springboot.blog.service;
 
 import com.springboot.blog.payload.CommentDto;
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.payload.PostResponse;
+
+import java.util.List;
 
 public interface CommentService {
     CommentDto createComment(long postId, CommentDto commentDto);
+
+    List<CommentDto> getCommentsByPostId(long postId);
+
+//    CommentResponse getAllComments(int pageNum, int pageSize,
+//                             String sortBy, String sortDir);
+
+      CommentDto getCommentById(long postId, long commentId);
+
+//    CommentDto updateCommentById(CommentDto postDto, long id);
+
+//    void deleteCommentById(long id);
 }
--- src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
@@ -2,13 +2,18 @@ package com.springboot.blog.service.impl;
 
 import com.springboot.blog.entity.Comment;
 import com.springboot.blog.entity.Post;
+import com.springboot.blog.exception.BlogAPIException;
 import com.springboot.blog.exception.ResourceNotFoundExceprion;
 import com.springboot.blog.payload.CommentDto;
 import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.repository.CommentRepository;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.CommentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentServiceImpl implements CommentService {
@@ -37,6 +42,33 @@ public class CommentServiceImpl implements CommentService {
         Comment newComment = commentRepository.save(comment);
 
         return mapToDTO(newComment);
+    }
+
+    @Override
+    public List<CommentDto> getCommentsByPostId(long postId) {
+        // retrieve comments by postId
+        List<Comment> comments = commentRepository.findByPostId(postId);
+
+        // convert list of comment entities to ist of comment DTOs
+        return comments.stream().map(comment -> mapToDTO(comment)).collect(Collectors.toList());
+    }
+
+    @Override
+    public CommentDto getCommentById(long postId, long commentId) {
+
+        // return Post entity by Id
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new ResourceNotFoundExceprion("Post", "id", postId)
+        );
+        // return Comment entity by Id
+        Comment comment = commentRepository.findById(commentId).orElseThrow(
+                () -> new ResourceNotFoundExceprion("Comment", "id", commentId)
+        );
+
+        if (!comment.getPost().getId().equals(post.getId())){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Comment does not belong to Post");
+        }
+        return mapToDTO(comment);
     }
 
     //convert entity to DTO
